2cd2694f2deae3aa09eb83150b71e9f6bb989b17
Nina <rumpilstilstkin@gmail.com>
2013-10-11 10:48:42 +1100

    Update lab03.c

--- labs/tbondarenko/lab03.c
+++ labs/tbondarenko/lab03.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// это не лабораторная 3, это пример с лекции. Если вы не помните задание,
+// вы вполне можете мне написать чтобы все уточнить
 int main(int argc, char *argv[], char *envp[])
 {
 	(void)execle("/bin/cat", "/bin/cat", "lab2.c" ,0 , envp);
