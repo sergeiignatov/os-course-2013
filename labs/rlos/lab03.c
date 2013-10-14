44bf00da0be376f9ce9b67defd36b3d5af036935
Los Roman <sin_ss@list.ru>
2013-10-14 22:26:04 +1100

    Update lab03.c
    Исправил код возврата в случае ошибки. Добавил нулевой аргумент в параметры execl для верной передачи аргументов.

--- labs/rlos/lab03.c
+++ labs/rlos/lab03.c
@@ -5,18 +5,8 @@
 int main(int argc, char* argv[], char* env[]) {
 	printf("--------------------LAB3--------------------\n");
 	printf("%d\n",argc);
-	// на счет проблемы с тем что вызванная программа принимает лишние аргументы 
-	// если вы посмотрите описание этой функции вы скорее всего увидете что то вроде
-	// The last parameter must always be 0. It is a NULL terminator.
-	// Since the argument list is variable we must have some way of telling C when it is to end.
-	// The NULL terminator does this job.
-	// Поэтому чтобы сказать что параметры закончились вызов execl в вашем случае должен выглядеть так
-	// execl(argv[1], argv[1], argv[0], 0); 
-	execl(argv[1], argv[1], argv[0]); 
+	execl(argv[1], argv[1], argv[0], 0); 
 	printf("Ошибка передачи управления\n");
-	// Так как следующие строки выполнятся только в случае неудачи смены контекста, 
-	//кажется немного странным что вы завершаете программу с кодом правильного завершения приложения exit(0)
 	printf("--------------------LAB3END--------------------\n");
-	exit(0);
+	exit(-1);
 }
-
