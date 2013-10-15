5bf659ba0d85d2dcfad6705471242a2cc5a24536
tanya-bondarenko <tanysha92@list.ru>
2013-10-15 16:00:51 +1100

    Rewrite

--- labs/tbondarenko/lab03.c
+++ labs/tbondarenko/lab03.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// это не лабораторная 3, это пример с лекции. Если вы не помните задание,
-// вы вполне можете мне написать чтобы все уточнить
 int main(int argc, char *argv[], char *envp[])
 {
-	(void)execle("/bin/cat", "/bin/cat", "lab2.c" ,0 , envp);
+	(void)execle("/home/jsmith/tanya/lab2", "/home/jsmith/tanya/lab2", 0 , envp);
 	printf("Error!!!!!");
 	exit(-1);
-	return 0;
+	
 }
