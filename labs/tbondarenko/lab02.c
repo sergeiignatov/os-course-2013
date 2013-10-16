3234eebbf58d8d6c57527f8bc600a271f7da8092
Nina <rumpilstilstkin@gmail.com>
2013-10-16 13:59:06 +1100

    Update lab02.c

--- labs/tbondarenko/lab02.c
+++ labs/tbondarenko/lab02.c
@@ -7,14 +7,15 @@ int main(int argc, char *argv[], char *envp[])
 	int i = 0;
 	int j = 0; 
 	while(i != argc ){
-	printf("%s\n" , argv[i]);
-	i++;
+		printf("%s\n" , argv[i]);
+		i++;
 	}
+	// "END это тоже строка)))"
 	printf("%c" , "END");
 	while(envp[ j ] != NULL){
-	printf("%s\n" , envp[ j ]);
-	j++;
+		printf("%s\n" , envp[ j ]);
+		j++;
 	}
 
-return 0;
+	return 0;
 }
