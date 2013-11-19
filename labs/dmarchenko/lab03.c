703a74bc52210177534991cf3ecd788db5e92988
Danil-Marchenko <theif_zone@mail.ru>
2013-11-19 23:59:54 +1100

    Добавленна 3 лабораторная, сделал Марченко Данил

---
+++ labs/dmarchenko/lab03.c
@@ -0,0 +1,12 @@
+﻿#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	printf("Будет выполнена программа %s\n", argv[1]);
+	printf("Argument count: %d\n\n",argc);
+	execle(argv[1], argv[1], argv[0], 0, envp);
+	printf("Ошибка!!! \n");
+	exit(-1);
+}
