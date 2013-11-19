539eeacd18bbe955b3988bb78aa24f97e8bf0896
Danil-Marchenko <theif_zone@mail.ru>
2013-11-20 00:02:57 +1100

    Добавленна 1 лабораторная, сделал Марченко Данил, старую из папки kabs можно убрать

---
+++ labs/dmarchenko/lab01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	pid_t pid;
+	pid = fork();
+	int i;
+	if (pid == 0)
+	{
+		printf("id сына: %d\n", getpid());
+		printf("id отца: %d\n", getppid());
+		for (i = 0; i < 10; i++)
+		{
+			printf("%d\n", i*2);
+		}
+		
+		return 0;
+	}
+	printf("id отца: %d\n", getpid());
+	printf("id сына: %d\n", pid);
+	for (i = 100; i > 90; i--)
+	{
+		printf("%d\n", i/2);
+	}
+	return 0;
+	
+}
