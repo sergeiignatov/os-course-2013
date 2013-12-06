b103f3b28f6b7a962b5dd7b05c558d8726974810
Dombr <viper_1666@inbox.ru>
2013-12-06 20:05:26 +1100

    Create lab02.c

---
+++ labs/adombrovskiy/lab02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[], char* env[]) {
+        printf("--------------------LAB2--------------------\n");
+        printf("%d\n" , argc);
+        int i;
+        for ( i = 0; i < argc; i++ )
+                printf("%s\n" , argv[i]);
+        printf("%s" , "--------------------end of argv--------------------\n");
+        i = 0;
+        while( env[i] != NULL ){
+                printf("%s\n" , env[ i ]);
+                i++;
+        }
+        printf("--------------------LAB2END--------------------\n");
+        exit(0);
+}
