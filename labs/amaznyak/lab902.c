6af5c73c7d2323eddd52633b813f9a98588709c6
Andrey Maznyak <andreymaznyak@gmail.com>
2014-01-13 09:12:46 +1100

    Update lab902.c

--- labs/amaznyak/lab902.c
+++ labs/amaznyak/lab902.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/shm.h>
+#include <sys/mman.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -12,29 +13,46 @@
 int main(){
 
 char *array;
-int md, flag;
-key_t key;
 int fd;
+sem_t *sema_n;
+ int ret,val;
+ 
 printf("%s : Im start \n", MYNAME);
 
 char pathname[] = "lab901.c";
 
-if((key = ftok(pathname, 0)) < 0){
-printf("%s : Can\'t generate key\n", MYNAME);
-}
-
-if((md = shmget(key, LIST_SIZE*sizeof(char), 0)) < 0){
-printf("%s : Can\'t find shared memory\n", MYNAME);
-//exit(-1);
-}
-
-if((array = (char *)shmat(md, NULL, 0)) == (char *)(-1)){
-printf("%s : Can't attach shared memory\n", MYNAME);
-}
+/* Получаем дескриптор общей памяти */
+  if ((shm_fd = shm_open("my_shm", O_CREAT | O_RDWR, 0666)) ==
+                                                          -1){
+    perror("cannot open");
+    return -1;
+  }
+ 
+  /* Устанавливаем размер общей памяти равным SHM_SIZE */
+  if (ftruncate(shm_fd, SHM_SIZE) != 0){
+    perror("cannot set size");
+    return -1;
+  }
+ 
+  /*
+   * Подключаем общую память в адресное пространство. Флаг
+   * MAP_SHARED говорит, что это подключение общей памяти.
+   */
+  if ((array = (char *) mmap(0, SHM_SIZE, PROT_WRITE, MAP_SHARED,
+               shm_fd, 0)) == MAP_FAILED){
+    perror("cannot mmap");
+    return -1;
+  }
+ 
+  /* Блокируем общую память. Не забываем про этот шаг */
+  if (mlock(vaddr, SHM_SIZE) != 0){
+    perror("cannot mlock");
+    return -1;
+  }
 
 int i;
 i = 0;
-printf("%s : code in lab701.c:\n", MYNAME);
+printf("%s : code in lab901.c:\n", MYNAME);
 printf("-----------------------------------------------------------\n\n");
 
 while (array[i] != EOF ){
@@ -43,10 +61,11 @@ while (array[i] != EOF ){
         ++i;
 }
 printf("%s \n", array);
-if(shmctl(md, IPC_RMID, NULL) < 0){
-printf("%s : Can't free shared memory\n", MYNAME);
 
-}
+munmap(vaddr, SHM_SIZE);
+close(shm_fd);
+/* Удаляем сегмент общей памяти */
+shm_unlink("my_shm");
 
 printf("%s : \nAll shared mamory released\n\n", MYNAME);
 
